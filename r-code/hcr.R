618f5dd2c241e057adc7c37a52d794172f9388e4
Christopher Cahill <you@example.com>
2022-01-03 14:04:00 -0800

    extracting draws for hcr

--- r-code/hcr.R
+++ r-code/hcr.R
@@ -13,13 +13,16 @@ library(purrr)
 list.files("fits/", full.names = TRUE)
 
 # pick a file 
-which_file <- "fits/lac_ste._anne_bh_cr_6.rds"
+# which_file <- "fits/lac_ste._anne_bh_cr_6.rds"
 
-# read fits into a big list in using map
-fit <-
-  which_file %>%
-  purrr::set_names(.) %>%
-  purrr::map(readRDS)
+# extract all the saved .stan fit names
+paths <- dir("fits/", pattern = "\\.rds$")
+paths <- paste0(getwd(), "/fits/", paths)
+fits <- map(paths, readRDS) %>%
+  set_names(paths)
+
+my_string <- names(fits)[10]
+fit <- fits[[which(names(fits)==my_string)]]
 
 #----------------------------------------------------------------------
 # ***N.B.***
@@ -28,3 +31,27 @@ fit <-
 # analysis
 # Thus, we will select 1990-2015 for these lakes for a 25 yr ref period
 #----------------------------------------------------------------------
+
+# Extract things from some (identical) rows of the posterior to 
+# preserve correlation structure
+
+n_draws <- 5 
+
+devs <- fit %>% 
+  spread_draws(R0, ar, br, sbr0_kick) %>%
+  sample_draws(n_draws)
+
+# index which draws were selected to ensure same draws for each simulation
+draw_idx <- unique(devs$.draw)
+
+w_devs <- fit %>%
+  spread_draws(w[year]) %>%
+  filter(.draw %in% draw_idx)
+
+v_devs <- fit %>%
+  spread_draws(v[period]) %>%
+  filter(.draw %in% draw_idx)
+
+F_devs <- fit %>%
+  spread_draws(F_vec[year]) %>%
+  filter(.draw %in% draw_idx)
