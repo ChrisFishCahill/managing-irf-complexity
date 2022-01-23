12dd0ef4cb68449a0c0809acab4b345dc8847829
Christopher Cahill <you@example.com>
2022-01-23 15:12:10 -0800

    bump pdf and plot code

--- r-code/plot_hcr.R
+++ r-code/plot_hcr.R
@@ -23,12 +23,13 @@ sim_files <- sim_files[grep("bh_cr_6_hcr_ass_int_1", sim_files)]
 # read in the data from all lakes used in Cahill et al. 2021
 data <- readRDS("data/BERTA-wide-0-25.rds")
 
-contract_lakes <- c("lac ste. anne", "baptiste lake", 
-                    "pigeon lake", "calling lake", 
-                    "moose lake", "lake newell"
+contract_lakes <- c(
+  "lac ste. anne", "baptiste lake",
+  "pigeon lake", "calling lake",
+  "moose lake", "lake newell"
 )
 
-#sim_files <- sim_files[grep("ricker_cr_6", sim_files)]
+# sim_files <- sim_files[grep("ricker_cr_6", sim_files)]
 
 names <- str_extract(
   string = sim_files,
@@ -72,13 +73,13 @@ p
 
 my_data <- NA
 wt_list <- big_list %>%
-  purrr::map(~ .x$wt_seqs) 
+  purrr::map(~ .x$wt_seqs)
 
-for(i in names(wt_list)){
+for (i in names(wt_list)) {
   lake_data <- wt_list[[i]]
-  long_data <- setNames(melt(lake_data), c('sim_yr','draw', 'wt'))
+  long_data <- setNames(melt(lake_data), c("sim_yr", "draw", "wt"))
   long_data$lake <- gsub("_", " ", i)
-  if(names(wt_list)[1]==i){
+  if (names(wt_list)[1] == i) {
     my_data <- long_data
   } else {
     my_data <- rbind(my_data, long_data)
@@ -106,10 +107,12 @@ p <-
     legend.title = element_blank(),
     legend.position = "none",
     plot.title = element_text(face = "bold", hjust = 0.5)
-  ) + 
-  geom_line(data = trace_data2, aes(x = sim_yr, y = wt), color = "black", size = 0.3, 
-            alpha = 0.4) + 
-  geom_line(data = trace_data, aes(x = sim_yr, y = wt), color = "black", size = 0.3) 
+  ) +
+  geom_line(
+    data = trace_data2, aes(x = sim_yr, y = wt), color = "black", size = 0.3,
+    alpha = 0.4
+  ) +
+  geom_line(data = trace_data, aes(x = sim_yr, y = wt), color = "black", size = 0.3)
 
 ggsave("plots/hcr_wts.pdf",
   width = 8,
@@ -121,72 +124,12 @@ ggsave("plots/hcr_wts.pdf",
 
 my_data <- NA
 yield_list <- big_list %>%
-  purrr::map(~ .x$tot_y) 
+  purrr::map(~ .x$tot_y)
 
 # these loops are r-binding the total yield matrices for each file into one big dataframe
-for(i in names(yield_list)){
+for (i in names(yield_list)) {
   lake_data <- yield_list[[i]]
-  long_data <- 
-    lake_data %>%
-  as.data.frame.table(., responseName = "value", dnn = c("cslope", "bmin")) %>%
-  rename(
-    "cslope" = "Var1",
-    "bmin" = "Var2"
-  ) %>%
-  mutate(
-    cslope = as.numeric(as.character(cslope)),
-    bmin = as.numeric(as.character(bmin)), 
-    lake = gsub("_", " ", i)
-  )
-  if(names(yield_list)[1]==i){
-    my_data <- long_data
-    } else {
-      my_data <- rbind(my_data, long_data)
-    }
-}
-# rm(my_data)
-
-plot_list <- vector('list', length(unique(my_data$lake)))
-for(i in unique(my_data$lake)){
-  highlight <- my_data %>%
-    filter(lake == i) %>%
-    filter(value == max(value))
-p1 <- my_data %>%
-  filter(lake == i) %>%
-  ggplot(aes(bmin, cslope, z = value)) +
-  geom_contour_filled(bins = 15) +
-  ggsidekick::theme_sleek() +
-  labs(fill = "Yield") +
-  geom_point(data = highlight, aes(x = bmin, y = cslope), size = 1.75, show.legend = F) +
-  scale_color_manual(values = c(NA, "black")) +
-  xlab("Limit reference biomass (kg)") + 
-  ggtitle(i) +
-  theme(
-    legend.title = element_blank(),
-    legend.position = "none",
-    plot.title = element_text(hjust = 0.5)
-  )  
-  #geom_vline(xintercept=10)
-  plot_list[[which(unique(my_data$lake) == i )]] <- p1 
-}
-
-bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=3)
-#bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=1)
-ggsave("plots/yield_isos.pdf", bigp,
-       width = 8,
-       height = 5
-)
-
-#----------------------------------------------------------------------
-# make the utility isopleth plots
-
-my_data <- NA
-hara_list <- big_list %>%
-  purrr::map(~ .x$tot_u) 
-
-for(i in names(hara_list)){
-  lake_data <- hara_list[[i]]
-  long_data <- 
+  long_data <-
     lake_data %>%
     as.data.frame.table(., responseName = "value", dnn = c("cslope", "bmin")) %>%
     rename(
@@ -195,10 +138,70 @@ for(i in names(hara_list)){
     ) %>%
     mutate(
       cslope = as.numeric(as.character(cslope)),
-      bmin = as.numeric(as.character(bmin)), 
+      bmin = as.numeric(as.character(bmin)),
       lake = gsub("_", " ", i)
     )
-  if(names(hara_list)[1]==i){
+  if (names(yield_list)[1] == i) {
+    my_data <- long_data
+  } else {
+    my_data <- rbind(my_data, long_data)
+  }
+}
+# rm(my_data)
+
+plot_list <- vector("list", length(unique(my_data$lake)))
+for (i in unique(my_data$lake)) {
+  highlight <- my_data %>%
+    filter(lake == i) %>%
+    filter(value == max(value))
+  p1 <- my_data %>%
+    filter(lake == i) %>%
+    ggplot(aes(bmin, cslope, z = value)) +
+    geom_contour_filled(bins = 15) +
+    ggsidekick::theme_sleek() +
+    labs(fill = "Yield") +
+    geom_point(data = highlight, aes(x = bmin, y = cslope), size = 1.75, show.legend = F) +
+    scale_color_manual(values = c(NA, "black")) +
+    xlab("Limit reference biomass (kg)") +
+    ggtitle(i) +
+    theme(
+      legend.title = element_blank(),
+      legend.position = "none",
+      plot.title = element_text(hjust = 0.5)
+    )
+  # geom_vline(xintercept=10)
+  plot_list[[which(unique(my_data$lake) == i)]] <- p1
+}
+
+bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol = 3)
+# bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=1)
+ggsave("plots/yield_isos.pdf", bigp,
+  width = 8,
+  height = 5
+)
+
+#----------------------------------------------------------------------
+# make the utility isopleth plots
+
+my_data <- NA
+hara_list <- big_list %>%
+  purrr::map(~ .x$tot_u)
+
+for (i in names(hara_list)) {
+  lake_data <- hara_list[[i]]
+  long_data <-
+    lake_data %>%
+    as.data.frame.table(., responseName = "value", dnn = c("cslope", "bmin")) %>%
+    rename(
+      "cslope" = "Var1",
+      "bmin" = "Var2"
+    ) %>%
+    mutate(
+      cslope = as.numeric(as.character(cslope)),
+      bmin = as.numeric(as.character(bmin)),
+      lake = gsub("_", " ", i)
+    )
+  if (names(hara_list)[1] == i) {
     my_data <- long_data
   } else {
     my_data <- rbind(my_data, long_data)
@@ -207,8 +210,8 @@ for(i in names(hara_list)){
 # rm(my_data)
 
 # make the utility isopleth plots
-plot_list <- vector('list', length(unique(my_data$lake)))
-for(i in unique(my_data$lake)){
+plot_list <- vector("list", length(unique(my_data$lake)))
+for (i in unique(my_data$lake)) {
   highlight <- my_data %>%
     filter(lake == i) %>%
     filter(value == max(value))
@@ -220,41 +223,42 @@ for(i in unique(my_data$lake)){
     labs(fill = "Utility") +
     geom_point(data = highlight, aes(x = bmin, y = cslope), size = 1.75, show.legend = F) +
     scale_color_manual(values = c(NA, "black")) +
-    xlab("Limit reference biomass (kg)") + 
+    xlab("Limit reference biomass (kg)") +
     ggtitle(i) +
     theme(
       legend.title = element_blank(),
       legend.position = "none",
       plot.title = element_text(hjust = 0.5)
     )
-  plot_list[[which(unique(my_data$lake) == i )]] <- p1 
+  plot_list[[which(unique(my_data$lake) == i)]] <- p1
 }
 
-bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=3)
-#bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=1)
+bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol = 3)
+# bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=1)
 
 ggsave("plots/hara_isos.pdf", bigp,
-       width = 8,
-       height = 5
+  width = 8,
+  height = 5
 )
 
 #----------------------------------------------------------------------
 # plot vulnerable biomass vs. yield for msy policy
 
 vb_list <- big_list %>%
-  purrr::map(~ .x$MSY_vB_fish) 
+  purrr::map(~ .x$MSY_vB_fish)
 
 names(vb_list) <- gsub("_", " ", names(vb_list))
 
 my_data <- tibble()
 
-for(i in names(vb_list)){
+for (i in names(vb_list)) {
   lake_data <- vb_list[[i]]
-  long_data <- tibble(MSY_vB = lake_data, 
-                      year = retro_initial_yr:(retro_initial_yr + n_repeats*length(retro_initial_yr:retro_terminal_yr) - 1), 
-                      lake = gsub("_", " ", i)
+  long_data <- tibble(
+    MSY_vB = lake_data,
+    year = retro_initial_yr:(retro_initial_yr + n_repeats * length(retro_initial_yr:retro_terminal_yr) - 1),
+    lake = gsub("_", " ", i)
   )
-  if(names(vb_list)[1]==i){
+  if (names(vb_list)[1] == i) {
     my_data <- long_data
   } else {
     my_data <- rbind(my_data, long_data)
@@ -263,19 +267,20 @@ for(i in names(vb_list)){
 
 # now get MSY yields
 yields_list <- big_list %>%
-  purrr::map(~ .x$MSY_yields) 
+  purrr::map(~ .x$MSY_yields)
 
-#names(yields_list) <- gsub("_", " ", names(yields_list))
+# names(yields_list) <- gsub("_", " ", names(yields_list))
 
 my_data2 <- tibble()
 
-for(i in names(yields_list)){
+for (i in names(yields_list)) {
   lake_data <- yields_list[[i]]
-  long_data <- tibble(MSY_yield = lake_data, 
-                      year = retro_initial_yr:(retro_initial_yr + 8*length(retro_initial_yr:retro_terminal_yr) - 1), 
-                      lake = gsub("_", " ", i)
+  long_data <- tibble(
+    MSY_yield = lake_data,
+    year = retro_initial_yr:(retro_initial_yr + 8 * length(retro_initial_yr:retro_terminal_yr) - 1),
+    lake = gsub("_", " ", i)
   )
-  if(names(vb_list)[1]==i){
+  if (names(vb_list)[1] == i) {
     my_data2 <- long_data
   } else {
     my_data2 <- rbind(my_data2, long_data)
@@ -283,7 +288,7 @@ for(i in names(yields_list)){
 }
 
 my_data$MSY_yields <- my_data2$MSY_yield
-my_data[which(my_data$year == max(my_data$year) ), c("MSY_vB", "MSY_yields")] = NA
+my_data[which(my_data$year == max(my_data$year)), c("MSY_vB", "MSY_yields")] <- NA
 
 # p <-
 #   my_data %>%
@@ -297,15 +302,15 @@ my_data[which(my_data$year == max(my_data$year) ), c("MSY_vB", "MSY_yields")] = 
 #     legend.title = element_blank(),
 #     legend.position = "none",
 #     plot.title = element_text(face = "bold", hjust = 0.5)
-#   ) + 
+#   ) +
 #   geom_line(aes(x=year, y=MSY_yields), colour="darkorange2", size=0.5)
 # p
-# 
+#
 # ggsave("plots/vb_yield_msy_policy.pdf",
 #        width = 8,
 #        height = 5
 # )
-# 
+#
 # p <-
 #   my_data %>%
 #   ggplot(aes(y = MSY_yields, x = MSY_vB)) +
@@ -318,30 +323,31 @@ my_data[which(my_data$year == max(my_data$year) ), c("MSY_vB", "MSY_yields")] = 
 #     legend.title = element_blank(),
 #     legend.position = "none",
 #     plot.title = element_text(face = "bold", hjust = 0.5)
-#   ) + 
-#   ggtitle("MSY policies") + 
+#   ) +
+#   ggtitle("MSY policies") +
 #   geom_abline() +
 #   geom_abline(slope=0.3)
 # p
 
 
 #----------------------------------------------------------------------
-# vulnerable biomass vs. yield for hara 
+# vulnerable biomass vs. yield for hara
 
 vb_list <- big_list %>%
-  purrr::map(~ .x$HARA_vB_fish) 
+  purrr::map(~ .x$HARA_vB_fish)
 
 names(vb_list) <- gsub("_", " ", names(vb_list))
 
 my_data <- tibble()
 
-for(i in names(vb_list)){
+for (i in names(vb_list)) {
   lake_data <- vb_list[[i]]
-  long_data <- tibble(Utility_vB = lake_data, 
-                      year = retro_initial_yr:(retro_initial_yr + 8*length(retro_initial_yr:retro_terminal_yr) - 1), 
-                      lake = gsub("_", " ", i)
+  long_data <- tibble(
+    Utility_vB = lake_data,
+    year = retro_initial_yr:(retro_initial_yr + 8 * length(retro_initial_yr:retro_terminal_yr) - 1),
+    lake = gsub("_", " ", i)
   )
-  if(names(hara_list)[1]==i){
+  if (names(hara_list)[1] == i) {
     my_data <- long_data
   } else {
     my_data <- rbind(my_data, long_data)
@@ -350,19 +356,20 @@ for(i in names(vb_list)){
 
 # now get hara yields
 yields_list <- big_list %>%
-  purrr::map(~ .x$HARA_yields) 
+  purrr::map(~ .x$HARA_yields)
 
-#names(yields_list) <- gsub("_", " ", names(yields_list))
+# names(yields_list) <- gsub("_", " ", names(yields_list))
 
 my_data2 <- tibble()
 
-for(i in names(yields_list)){
+for (i in names(yields_list)) {
   lake_data <- yields_list[[i]]
-  long_data <- tibble(HARA_yield = lake_data, 
-                      year = retro_initial_yr:(retro_initial_yr + 8*length(retro_initial_yr:retro_terminal_yr) - 1), 
-                      lake = gsub("_", " ", i)
+  long_data <- tibble(
+    HARA_yield = lake_data,
+    year = retro_initial_yr:(retro_initial_yr + 8 * length(retro_initial_yr:retro_terminal_yr) - 1),
+    lake = gsub("_", " ", i)
   )
-  if(names(hara_list)[1]==i){
+  if (names(hara_list)[1] == i) {
     my_data2 <- long_data
   } else {
     my_data2 <- rbind(my_data2, long_data)
@@ -370,8 +377,8 @@ for(i in names(yields_list)){
 }
 
 my_data$HARA_yield <- my_data2$HARA_yield
-my_data[which(my_data$year == max(my_data$year) ), c("Utility_vB", "HARA_yield")] = NA
-  
+my_data[which(my_data$year == max(my_data$year)), c("Utility_vB", "HARA_yield")] <- NA
+
 # p <-
 #   my_data %>%
 #   ggplot(aes(x = year, y = Utility_vB)) +
@@ -384,13 +391,13 @@ my_data[which(my_data$year == max(my_data$year) ), c("Utility_vB", "HARA_yield")
 #     legend.title = element_blank(),
 #     legend.position = "none",
 #     plot.title = element_text(face = "bold", hjust = 0.5)
-#   ) + 
+#   ) +
 #   geom_line(aes(x=year, y=HARA_yield), colour="darkorange2", size=0.5)
 # p
 
 ggsave("plots/vb_yield_hara_policy.pdf",
-       width = 8,
-       height = 5
+  width = 8,
+  height = 5
 )
 
 # p <-
@@ -405,65 +412,65 @@ ggsave("plots/vb_yield_hara_policy.pdf",
 #     legend.title = element_blank(),
 #     legend.position = "none",
 #     plot.title = element_text(face = "bold", hjust = 0.5)
-#   ) + 
+#   ) +
 #   ggtitle("HARA policies") +
 #   geom_abline() +
 #   geom_abline(slope=0.3) +
 #   geom_abline(slope=0.2)
 # p
-# 
+#
 #----------------------------------------------------------------------
 # Let's make a plot of the frontier of tradeoffs between utility and yield
-# policies for simple linear hcrs 
+# policies for simple linear hcrs
 #----------------------------------------------------------------------
 
 tot_y_list <- big_list %>%
-  purrr::map(~ .x$tot_y) 
+  purrr::map(~ .x$tot_y)
 
 tot_u_list <- big_list %>%
-  purrr::map(~ .x$tot_u) 
+  purrr::map(~ .x$tot_u)
 
-#tot_y <- tot_y_list[["baptiste_lake"]]
-#tot_u <- tot_u_list[["baptiste_lake"]]
+# tot_y <- tot_y_list[["baptiste_lake"]]
+# tot_u <- tot_u_list[["baptiste_lake"]]
 
-# get all the yield data 
+# get all the yield data
 my_y_data <- tibble()
-for(i in names(tot_y_list)){
+for (i in names(tot_y_list)) {
   lake_data <- tot_y_list[[i]]
   y_df <- lake_data %>%
     as.data.frame.table(., responseName = "x_yield", dnn = c("cslope", "bmin")) %>%
     rename(
       "cslope" = "Var1",
-      "bmin" = "Var2", 
+      "bmin" = "Var2",
     ) %>%
     mutate(
       cslope = as.numeric(as.character(cslope)),
-      bmin = as.numeric(as.character(bmin)), 
+      bmin = as.numeric(as.character(bmin)),
       lake = gsub("_", " ", i)
     )
-  if(names(tot_y_list)[1]==i){
+  if (names(tot_y_list)[1] == i) {
     my_y_data <- y_df
   } else {
     my_y_data <- rbind(my_y_data, y_df)
   }
 }
 
-# get all the utility data 
+# get all the utility data
 my_u_data <- tibble()
-for(i in names(tot_u_list)){
+for (i in names(tot_u_list)) {
   lake_data <- tot_u_list[[i]]
   u_df <- lake_data %>%
     as.data.frame.table(., responseName = "y_utility", dnn = c("cslope", "bmin")) %>%
     rename(
       "cslope" = "Var1",
-      "bmin" = "Var2", 
+      "bmin" = "Var2",
     ) %>%
     mutate(
       cslope = as.numeric(as.character(cslope)),
-      bmin = as.numeric(as.character(bmin)), 
+      bmin = as.numeric(as.character(bmin)),
       lake = gsub("_", " ", i)
     )
-  if(names(tot_u_list)[1]==i){
+  if (names(tot_u_list)[1] == i) {
     my_u_data <- u_df
   } else {
     my_u_data <- rbind(my_u_data, u_df)
@@ -471,57 +478,74 @@ for(i in names(tot_u_list)){
 }
 
 frontier <- left_join(my_y_data, my_u_data)
+frontier$bmin <- round(frontier$bmin, 0)
 
-plot_list <- vector('list', length(unique(frontier$lake)))
-for(i in unique(frontier$lake)){
+plot_list <- vector("list", length(unique(frontier$lake)))
+for (i in unique(frontier$lake)) {
   yint <- frontier %>%
     filter(lake == i) %>%
-    summarize(value = max(y_utility)*0.8)
+    summarize(value = max(y_utility) * 0.8)
+  n_bins <- frontier %>%
+    filter(
+      lake == i,
+      x_yield > 0,
+      bmin <= 20,
+    ) %>%
+    summarize(n_bins = length(unique(bmin)))
+  my_colors <- colorRampPalette(brewer.pal(8, "Dark2"))(n_bins$n_bins)
   p1 <- frontier %>%
-    filter(lake == i) %>%
+    filter(
+      lake == i,
+      x_yield > 0,
+      bmin <= 20
+    ) %>%
     ggplot(aes(x = x_yield, y = y_utility, z = bmin, colour = as.factor(bmin))) +
-    geom_line() + 
+    geom_line() +
     ggsidekick::theme_sleek() +
     geom_point() +
-    scale_color_viridis(discrete=TRUE) +
-    #scale_color_manual(values = viridis(n = num_colors)) +
+    scale_color_manual(values = my_colors) +
+    # scale_color_brewer(palette = "Set2") +
     xlab("Yield") +
-    ylab("Utility") + 
+    ylab("Utility") +
     ggtitle(i) +
+    labs(color = "blim") +
     theme(
-      legend.title = element_blank(),
-      legend.position = "none",
+      # legend.title = element_blank(),
+      # legend.box="vertical",
+      legend.position = "right",
       plot.title = element_text(hjust = 0.5)
-    ) + 
-    geom_hline(yintercept = yint$value)
-  plot_list[[which(unique(my_data$lake) == i )]] <- p1 
+    ) +
+    guides(fill=guide_legend(ncol=4,bycol=TRUE)) +
+    geom_hline(yintercept = yint$value, linetype = "dashed")
+  plot_list[[which(unique(my_data$lake) == i)]] <- p1
 }
 
-bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=3)
+bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol = 3)
 
 ggsave("plots/frontier.pdf", bigp,
-       width = 8,
-       height = 5
+  width = 11.0,
+  height = 8
 )
 
 #----------------------------------------------------------------------
-# Extra plotting code below:  
+# Extra plotting code below:
 #----------------------------------------------------------------------
 
 yields_list <- big_list %>%
-  purrr::map(~ .x$MSY_yields) 
+  purrr::map(~ .x$MSY_yields)
 
-#names(yields_list) <- gsub("_", " ", names(yields_list))
+# names(yields_list) <- gsub("_", " ", names(yields_list))
 
 my_data <- NA
 
-for(i in names(yields_list)){
+for (i in names(yields_list)) {
   lake_data <- yields_list[[i]]
-  long_data <- tibble(MSY_yield = lake_data, 
-                      year = retro_initial_yr:(retro_initial_yr + 8*length(retro_initial_yr:retro_terminal_yr) - 1), 
-                      lake = gsub("_", " ", i)
+  long_data <- tibble(
+    MSY_yield = lake_data,
+    year = retro_initial_yr:(retro_initial_yr + 8 * length(retro_initial_yr:retro_terminal_yr) - 1),
+    lake = gsub("_", " ", i)
   )
-  if(names(hara_list)[1]==i){
+  if (names(hara_list)[1] == i) {
     my_data <- long_data
   } else {
     my_data <- rbind(my_data, long_data)
@@ -534,7 +558,7 @@ p <-
   geom_line(colour = "#80b1d3", size = 0.5, alpha = 1) +
   ylab("Maximum yields from MSY policy") +
   xlab("Year") +
-  facet_wrap(~lake, scales="free") +
+  facet_wrap(~lake, scales = "free") +
   ggsidekick::theme_sleek() +
   theme(
     legend.title = element_blank(),
@@ -544,8 +568,8 @@ p <-
 p
 
 ggsave("plots/yields.pdf",
-       width = 8,
-       height = 5
+  width = 8,
+  height = 5
 )
 
 
