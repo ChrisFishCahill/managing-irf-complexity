e59e62f88435df27dda30b05c57c9c1c1f1e8d9a
Christopher Cahill <you@example.com>
2022-03-22 14:06:51 -0700

    tuesday afternoon edits

--- r-code/plot_hcr.R
+++ r-code/plot_hcr.R
@@ -122,9 +122,46 @@ p
 #   height = 5
 # )
 
+# ggsave("plots/hcr_wts.jpeg",
+#   width = 8,
+#   height = 5
+# )
+
 my_data %>% filter(lake == "pigeon lake") %>%
   ggplot(aes(x = sim_yr, y = wt, colour = lake, group = draw)) +
   geom_line(color = "#80b1d3", size = 0.05, alpha = 0.35)
+
+trace_data2 <- 
+  trace_data2 %>%
+  filter(lake == "baptiste lake")
+
+trace_data <- 
+  trace_data %>%
+  filter(lake == "baptiste lake")
+p <-
+  my_data %>%
+  filter(lake == "baptiste lake") %>%
+  ggplot(aes(x = sim_yr, y = wt, colour = lake, group = draw)) +
+  geom_line(color = "#80b1d3", size = 0.05, alpha = 0.35) +
+  ylab(expression(ln(w[t]))) +
+  xlab("Year") +
+  ggsidekick::theme_sleek() +
+  ggtitle("Recruitment anomaly sequences used for Baptiste Lake") + 
+  theme(
+    plot.title = element_text(face = "bold", hjust = 0.5)
+  ) +
+  geom_line(
+    data = trace_data2, aes(x = sim_yr, y = wt), color = "black", size = 0.75,
+    alpha = 0.4
+  ) +
+  geom_line(data = trace_data, aes(x = sim_yr, y = wt), color = "black", size = 0.75)
+p
+
+ggsave("plots/hcr_wts_baptiste.jpeg",
+  width = 8,
+  height = 5
+)
+
 
 #----------------------------------------------------------------------
 # make the yield isopleth plots
@@ -188,6 +225,11 @@ ggsave("plots/yield_isos_ass_int_1_dmort_0.3_sd_0.4.pdf", bigp,
   height = 5
 )
 
+ggsave("plots/yield_isos_ass_int_1_dmort_0.3_sd_0.4.jpeg", bigp,
+       width = 7.3,
+       height = 4
+)
+
 #----------------------------------------------------------------------
 # make the utility isopleth plots
 
@@ -244,10 +286,16 @@ for (i in unique(my_data$lake)) {
 bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol = 3)
 # bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=1)
 
-ggsave("plots/hara_isos.pdf", bigp,
-  width = 8,
-  height = 5
+# ggsave("plots/hara_isos.pdf", bigp,
+#   width = 8,
+#   height = 5
+# )
+
+ggsave("plots/hara_isos_ass_int_1_dmort_0.3_sd_0.4.jpeg", bigp,
+       width = 7.3,
+       height = 4
 )
+
 
 #----------------------------------------------------------------------
 # plot vulnerable biomass vs. yield for msy policy
@@ -1446,3 +1494,576 @@ max_yields %>%
   ggtitle("HARA objective") + 
   scale_y_continuous(limits=c(0,3)) + 
   scale_x_continuous(limits=c(0,3))
+
+
+#######################################################################################################
+#######################################################################################################
+#######################################################################################################
+
+#Deliverable plots
+
+#----------------------------------------------------------------------
+# hcr plots
+#----------------------------------------------------------------------
+
+library(paletteer)
+library(viridis)
+library(purrr)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(gridExtra)
+library(reshape2)
+library(RColorBrewer)
+library(grid)
+
+retro_initial_yr <- 1990 # initial year for retrospective analysis
+retro_terminal_yr <- 2015
+n_repeats <- 8
+
+#----------------------------------------------------------------------
+# read in the hcr sim files
+sim_files <- list.files("sims/", full.names = TRUE)
+sim_files <- sim_files[grep("bh_cr_6_hcr_ass_int_1_sd_0.4_d_mort_0.3", sim_files)]
+
+# read in the data from all lakes used in Cahill et al. 2021
+data <- readRDS("data/BERTA-wide-0-25.rds")
+
+contract_lakes <- c(
+  "lac ste. anne", "baptiste lake",
+  "pigeon lake", "calling lake",
+  "moose lake", "lake newell"
+)
+
+# sim_files <- sim_files[grep("ricker_cr_6", sim_files)]
+
+names <- str_extract(
+  string = sim_files,
+  pattern = "(?<=sims/).*(?=_bh|_ricker)"
+)
+
+big_list <-
+  sim_files %>%
+  purrr::set_names(names) %>%
+  purrr::map(readRDS)
+
+all_posts <-
+  big_list %>%
+  purrr::map_dfr(~ .x$post, .id = "lake") %>%
+  mutate(lake = gsub("_", " ", lake))
+
+#----------------------------------------------------------------------
+# plot the recruitment anomaly w(t) sequences estimated by BERTA and used
+# for hcr simulation
+
+p <-
+  all_posts %>%
+  ggplot(aes(x = year, y = w, colour = lake, group = .draw)) +
+  geom_line(colour = "#80b1d3", size = 0.05, alpha = 0.35) +
+  ylab(expression(ln(w[t]))) +
+  xlab("Year") +
+  facet_wrap(~lake) +
+  ggsidekick::theme_sleek() +
+  ggtitle("Recruitment anomaly sequences used for harvest control rule development") +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "none",
+    plot.title = element_text(face = "bold", hjust = 0.5)
+  )
+p
+
+# ggsave("plots/hcr_wts.pdf",
+#   width = 8,
+#   height = 5
+# )
+
+my_data <- NA
+wt_list <- big_list %>%
+  purrr::map(~ .x$wt_seqs)
+
+for (i in names(wt_list)) {
+  lake_data <- wt_list[[i]]
+  long_data <- setNames(melt(lake_data), c("sim_yr", "draw", "wt"))
+  long_data$lake <- gsub("_", " ", i)
+  if (names(wt_list)[1] == i) {
+    my_data <- long_data
+  } else {
+    my_data <- rbind(my_data, long_data)
+  }
+}
+my_data$sim_yr <- my_data$sim_yr + 1989
+
+trace_data <- my_data %>%
+  filter(draw == 1) %>%
+  filter(sim_yr <= retro_terminal_yr)
+
+trace_data2 <- my_data %>%
+  filter(draw == 1)
+
+p <-
+  my_data %>%
+  ggplot(aes(x = sim_yr, y = wt, colour = lake, group = draw)) +
+  geom_line(color = "#80b1d3", size = 0.05, alpha = 0.35) +
+  ylab(expression(ln(w[t]))) +
+  xlab("Year") +
+  facet_wrap(~lake) +
+  ggsidekick::theme_sleek() +
+  ggtitle("Recruitment anomaly sequences used for harvest control rule development") +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "none",
+    plot.title = element_text(face = "bold", hjust = 0.5)
+  ) +
+  geom_line(
+    data = trace_data2, aes(x = sim_yr, y = wt), color = "black", size = 0.3,
+    alpha = 0.4
+  ) +
+  geom_line(data = trace_data, aes(x = sim_yr, y = wt), color = "black", size = 0.3)
+p
+
+ggsave("plots/hcr_wts.jpeg",
+  width = 8,
+  height = 5
+)
+
+trace_data2 <- 
+  trace_data2 %>%
+  filter(lake == "baptiste lake")
+
+trace_data <- 
+  trace_data %>%
+  filter(lake == "baptiste lake")
+p <-
+  my_data %>%
+  filter(lake == "baptiste lake") %>%
+  ggplot(aes(x = sim_yr, y = wt, colour = lake, group = draw)) +
+  geom_line(color = "#80b1d3", size = 0.05, alpha = 0.35) +
+  ylab(expression(ln(w[t]))) +
+  xlab("Year") +
+  ggsidekick::theme_sleek() +
+  ggtitle("Recruitment anomaly sequences used for Baptiste Lake") + 
+  theme(
+    plot.title = element_text(face = "bold", hjust = 0.5)
+  ) +
+  geom_line(
+    data = trace_data2, aes(x = sim_yr, y = wt), color = "black", size = 0.75,
+    alpha = 0.4
+  ) +
+  geom_line(data = trace_data, aes(x = sim_yr, y = wt), color = "black", size = 0.75)
+p
+
+ggsave("plots/hcr_wts_baptiste.jpeg",
+       width = 8,
+       height = 5
+)
+
+
+#----------------------------------------------------------------------
+# make the yield isopleth plots
+
+my_data <- NA
+yield_list <- big_list %>%
+  purrr::map(~ .x$tot_y)
+
+# these loops are r-binding the total yield matrices for each file into one big dataframe
+for (i in names(yield_list)) {
+  lake_data <- yield_list[[i]]
+  long_data <-
+    lake_data %>%
+    as.data.frame.table(., responseName = "value", dnn = c("cslope", "bmin")) %>%
+    rename(
+      "cslope" = "Var1",
+      "bmin" = "Var2"
+    ) %>%
+    mutate(
+      cslope = as.numeric(as.character(cslope)),
+      bmin = as.numeric(as.character(bmin)),
+      lake = gsub("_", " ", i)
+    )
+  if (names(yield_list)[1] == i) {
+    my_data <- long_data
+  } else {
+    my_data <- rbind(my_data, long_data)
+  }
+}
+# rm(my_data)
+
+plot_list <- vector("list", length(unique(my_data$lake)))
+for (i in unique(my_data$lake)) {
+  highlight <- my_data %>%
+    filter(lake == i) %>%
+    filter(value == max(value))
+  p1 <- my_data %>%
+    filter(lake == i) %>%
+    ggplot(aes(bmin, cslope, z = value)) +
+    geom_contour_filled(bins = 15) +
+    ggsidekick::theme_sleek() +
+    labs(fill = "Yield") +
+    geom_point(data = highlight, aes(x = bmin, y = cslope), size = 1.75, show.legend = F) +
+    scale_color_manual(values = c(NA, "black")) +
+    xlab("Limit reference biomass (kg)") +
+    ggtitle(i) +
+    theme(
+      legend.title = element_blank(),
+      legend.position = "none",
+      plot.title = element_text(hjust = 0.5)
+    )
+  # geom_vline(xintercept=10)
+  plot_list[[which(unique(my_data$lake) == i)]] <- p1
+}
+
+bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol = 3)
+
+# bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=1)
+ggsave("plots/yield_isos_ass_int_1_dmort_0.3_sd_0.4.pdf", bigp,
+       width = 8,
+       height = 5
+)
+
+ggsave("plots/yield_isos_ass_int_1_dmort_0.3_sd_0.4.jpeg", bigp,
+       width = 7.3,
+       height = 4
+)
+
+#----------------------------------------------------------------------
+# make the utility isopleth plots
+
+my_data <- NA
+hara_list <- big_list %>%
+  purrr::map(~ .x$tot_u)
+
+for (i in names(hara_list)) {
+  lake_data <- hara_list[[i]]
+  long_data <-
+    lake_data %>%
+    as.data.frame.table(., responseName = "value", dnn = c("cslope", "bmin")) %>%
+    rename(
+      "cslope" = "Var1",
+      "bmin" = "Var2"
+    ) %>%
+    mutate(
+      cslope = as.numeric(as.character(cslope)),
+      bmin = as.numeric(as.character(bmin)),
+      lake = gsub("_", " ", i)
+    )
+  if (names(hara_list)[1] == i) {
+    my_data <- long_data
+  } else {
+    my_data <- rbind(my_data, long_data)
+  }
+}
+# rm(my_data)
+
+# make the utility isopleth plots
+plot_list <- vector("list", length(unique(my_data$lake)))
+for (i in unique(my_data$lake)) {
+  highlight <- my_data %>%
+    filter(lake == i) %>%
+    filter(value == max(value))
+  p1 <- my_data %>%
+    filter(lake == i) %>%
+    ggplot(aes(bmin, cslope, z = value)) +
+    geom_contour_filled(bins = 15) +
+    ggsidekick::theme_sleek() +
+    labs(fill = "Utility") +
+    geom_point(data = highlight, aes(x = bmin, y = cslope), size = 1.75, show.legend = F) +
+    scale_color_manual(values = c(NA, "black")) +
+    xlab("Limit reference biomass (kg)") +
+    ggtitle(i) +
+    theme(
+      legend.title = element_blank(),
+      legend.position = "none",
+      plot.title = element_text(hjust = 0.5)
+    )
+  plot_list[[which(unique(my_data$lake) == i)]] <- p1
+}
+
+bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol = 3)
+# bigp <- gridExtra::grid.arrange(grobs = plot_list, ncol=1)
+
+# ggsave("plots/hara_isos.pdf", bigp,
+#   width = 8,
+#   height = 5
+# )
+
+ggsave("plots/hara_isos_ass_int_1_dmort_0.3_sd_0.4.jpeg", bigp,
+       width = 7.3,
+       height = 4
+)
+
+#----------------------------------------------------------------------------------------
+# let's see if we can visualize hcrs across simulation runs :(
+# read in the hcr sim files
+#-----------------------------------------------------------------------------------------
+sim_files <- list.files("sims/", full.names = TRUE)
+sim_files <- sim_files[grep("bh_cr_6_hcr", sim_files)]
+sim_files <- sim_files[grep("sd_0.4_d_mort_0.3", sim_files)] 
+sim_files <- sim_files[-grep("precautionary", sim_files)]
+# sim_files <- sim_files[grep("bh_cr_6_hcr_ass_int_1_sd_0.4_d_mort_0.3", sim_files)]
+
+sim_files
+
+names <- str_extract(
+  string = sim_files,
+  pattern = "(?<=sims/).*(?=_bh|_ricker)"
+)
+
+ass_ints <- str_extract(
+  string = sim_files,
+  pattern = "(?<=hcr_).*(?=_sd)"
+)
+
+new_names <- paste(names, ass_ints, sep="_")
+
+big_list <-
+  sim_files %>%
+  purrr::set_names(new_names) %>%
+  purrr::map(readRDS)
+
+#extract best yield cslope and bmin for each 
+names(big_list)
+
+my_data <- NA
+yield_list <- big_list %>%
+  purrr::map(~ .x$tot_y)
+
+# these loops are r-binding the total yield matrices for each file into one big dataframe
+for (i in names(yield_list)) {
+  lake_data <- yield_list[[i]]
+  long_data <-
+    lake_data %>%
+    as.data.frame.table(., responseName = "value", dnn = c("cslope", "bmin")) %>%
+    rename(
+      "cslope" = "Var1",
+      "bmin" = "Var2"
+    ) %>%
+    mutate(
+      cslope = as.numeric(as.character(cslope)),
+      bmin = as.numeric(as.character(bmin)),
+      lake = gsub("_", " ", i)
+    )
+  if (names(yield_list)[1] == i) {
+    my_data <- long_data
+  } else {
+    my_data <- rbind(my_data, long_data)
+  }
+}
+
+res_table <- data.frame(bmin = rep(NA, length(unique(my_data$lake))), 
+                        cslope = rep(NA, length(unique(my_data$lake))), 
+                        lake = rep(NA, length(unique(my_data$lake))),
+                        value = rep(NA, length(unique(my_data$lake))))
+
+for (i in unique(my_data$lake)) {
+  highlight <- my_data %>%
+    filter(lake == i) %>%
+    filter(value == max(value))
+  counter <- which(unique(my_data$lake)==i) 
+  res_table$bmin[counter] <- highlight$bmin
+  res_table$cslope[counter] <- highlight$cslope
+  res_table$lake[counter] <- highlight$lake
+  res_table$value[counter] <- highlight$value
+}
+res_table$ass_ints <- ass_ints
+res_table$lake <- names
+
+ass_ints <- str_extract(
+  string = res_table$ass_ints,
+  pattern = "(?<=ass_int_).*"
+)
+
+res_table$ass_ints <- as.numeric(ass_ints)
+
+#re-order based on ass_ints
+res_table <- 
+  res_table %>%
+  group_by(lake) %>%
+  arrange(ass_ints, .by_group=T)
+
+p <- res_table %>%
+  ggplot(aes(x=ass_ints, y=value, colour=lake)) + 
+  geom_point() + 
+  geom_line() + 
+  scale_x_continuous(breaks=c(1,3,5,10)) + 
+  ggtitle("Performance (MAY) vs. assessment interval, sd = 0.4, dmort = 0.3") + 
+  ylab("Yield") + 
+  xlab("Assessment interval (yrs)") + 
+  ggsidekick::theme_sleek() 
+
+ggsave("plots/performance_v_assint_sd_0.4_dmort_0.3.jpeg", p,
+       width = 6,
+       height = 4
+)
+  
+
+res_table$y_int <- - res_table$cslope*res_table$bmin
+
+res_table$lake <- gsub("_", " ", res_table$lake)
+
+my_colors <- colorRampPalette(brewer.pal(8, "Greys"))(length(unique(res_table$ass_ints)))
+#my_colors <- c('#7b3294','#c2a5cf','#a6dba0','#008837')
+p <- 
+  res_table %>%
+  ggplot() + 
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 50)) + 
+  scale_x_continuous(expand = c(0, 0), limits = c(0, 50), 
+                     breaks = c(0,5,10,15,20,25,30,35,40,45, 50)) + 
+  geom_abline(aes(intercept = y_int, slope = cslope,
+                  colour=as.factor(ass_ints)), size=1.15) +
+  ggsidekick::theme_sleek() + 
+  xlab("Biomass Vulnerable to Fishing (kg/ha)") +
+  ylab("TAC (kg/ha)") +
+  theme(axis.text=element_text(size=8), 
+        panel.spacing = unit(1.1, "lines"), 
+        plot.title = element_text(face = "bold", hjust = 0.5), 
+        legend.title.align=0.5
+  ) +
+  facet_wrap(~lake) +
+  labs(color = "Assessment \ninterval") +
+  scale_colour_grey(start = 0) + 
+  #scale_color_manual(values = my_colors) +
+  ggtitle("Best linear HCRs for MAY objective given discard mortality = 0.3 and survey sd = 0.4")
+p
+
+ggsave("plots/yield_hcr_sensitivity_sd_0.4_dmort_0.3.jpeg", p,
+       width = 7.3,
+       height = 4
+)
+
+
+#-----------------------------------------------------------------------------------------
+#now do it for HARA
+
+sim_files <- list.files("sims/", full.names = TRUE)
+sim_files <- sim_files[grep("bh_cr_6_hcr", sim_files)]
+sim_files <- sim_files[grep("sd_0.4_d_mort_0.3", sim_files)] 
+sim_files <- sim_files[-grep("precautionary", sim_files)]
+# sim_files <- sim_files[grep("bh_cr_6_hcr_ass_int_1_sd_0.4_d_mort_0.3", sim_files)]
+
+sim_files
+
+names <- str_extract(
+  string = sim_files,
+  pattern = "(?<=sims/).*(?=_bh|_ricker)"
+)
+
+ass_ints <- str_extract(
+  string = sim_files,
+  pattern = "(?<=hcr_).*(?=_sd)"
+)
+
+new_names <- paste(names, ass_ints, sep="_")
+
+big_list <-
+  sim_files %>%
+  purrr::set_names(new_names) %>%
+  purrr::map(readRDS)
+
+#extract best yield cslope and bmin for each 
+names(big_list)
+
+my_data <- NA
+yield_list <- big_list %>%
+  purrr::map(~ .x$tot_u) #note not tot_y 
+
+# these loops are r-binding the total yield matrices for each file into one big dataframe
+for (i in names(yield_list)) {
+  lake_data <- yield_list[[i]]
+  long_data <-
+    lake_data %>%
+    as.data.frame.table(., responseName = "value", dnn = c("cslope", "bmin")) %>%
+    rename(
+      "cslope" = "Var1",
+      "bmin" = "Var2"
+    ) %>%
+    mutate(
+      cslope = as.numeric(as.character(cslope)),
+      bmin = as.numeric(as.character(bmin)),
+      lake = gsub("_", " ", i)
+    )
+  if (names(yield_list)[1] == i) {
+    my_data <- long_data
+  } else {
+    my_data <- rbind(my_data, long_data)
+  }
+}
+
+res_table <- data.frame(bmin = rep(NA, length(unique(my_data$lake))), 
+                        cslope = rep(NA, length(unique(my_data$lake))), 
+                        lake = rep(NA, length(unique(my_data$lake))),
+                        value = rep(NA, length(unique(my_data$lake))))
+
+for (i in unique(my_data$lake)) {
+  highlight <- my_data %>%
+    filter(lake == i) %>%
+    filter(value == max(value))
+  counter <- which(unique(my_data$lake)==i) 
+  res_table$bmin[counter] <- highlight$bmin
+  res_table$cslope[counter] <- highlight$cslope
+  res_table$lake[counter] <- highlight$lake
+  res_table$value[counter] <- highlight$value
+}
+res_table$ass_ints <- ass_ints
+res_table$lake <- names
+
+ass_ints <- str_extract(
+  string = res_table$ass_ints,
+  pattern = "(?<=ass_int_).*"
+)
+
+res_table$ass_ints <- as.numeric(ass_ints)
+
+#re-order based on ass_ints
+res_table <- 
+  res_table %>%
+  group_by(lake) %>%
+  arrange(ass_ints, .by_group=T)
+
+p <- res_table %>%
+  ggplot(aes(x=ass_ints, y=value, colour=lake)) + 
+  geom_point() + 
+  geom_line() + 
+  scale_x_continuous(breaks=c(1,3,5,10)) + 
+  ggtitle("Performance (HARA) vs. assessment interval, sd = 0.4, dmort = 0.3") + 
+  ylab("Yield") + 
+  xlab("Assessment interval (yrs)") + 
+  ggsidekick::theme_sleek() 
+
+ggsave("plots/performance_hara_v_assint_sd_0.4_dmort_0.3.jpeg", p,
+       width = 6,
+       height = 4
+)
+res_table$y_int <- - res_table$cslope*res_table$bmin
+
+res_table$lake <- gsub("_", " ", res_table$lake)
+
+my_colors <- colorRampPalette(brewer.pal(8, "Greys"))(length(unique(res_table$ass_ints)))
+#my_colors <- c('#7b3294','#c2a5cf','#a6dba0','#008837')
+p <- 
+  res_table %>%
+  ggplot() + 
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 50)) + 
+  scale_x_continuous(expand = c(0, 0), limits = c(0, 50), 
+                     breaks = c(0,5,10,15,20,25,30,35,40,45, 50)) + 
+  geom_abline(aes(intercept = y_int, slope = cslope,
+                  colour=as.factor(ass_ints)), size=1.15) +
+  ggsidekick::theme_sleek() + 
+  xlab("Biomass Vulnerable to Fishing (kg/ha)") +
+  ylab("TAC (kg/ha)") +
+  theme(axis.text=element_text(size=8), 
+        panel.spacing = unit(1.1, "lines"), 
+        plot.title = element_text(face = "bold", hjust = 0.5), 
+        legend.title.align=0.5
+  ) +
+  facet_wrap(~lake) +
+  labs(color = "Assessment \ninterval") +
+  scale_colour_grey(start = 0) + 
+  #scale_color_manual(values = my_colors) +
+  ggtitle("Best linear HCRs for HARA objective given discard mortality = 0.3 and survey sd = 0.4")
+p
+
+ggsave("plots/hara_hcr_sensitivity_sd_0.4_dmort_0.3.jpeg", p,
+       width = 7.3,
+       height = 4
+)
+
