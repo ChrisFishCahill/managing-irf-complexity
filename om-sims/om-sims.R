1f12978d3381f83407ab39c8d55897cbaec1cec9
Chris Cahill <christopherfishcahill@gmail.com>
2022-10-07 12:24:12 -0400

    bump omniscient manager simulation example

---
+++ om-sims/om-sims.R
@@ -0,0 +1,85 @@
+#-----------------------------------------------------------------------
+# tranlate Carl Walter's omniscient manager simulations  for spasmodic stocks
+#                          from excel to R
+#                         Cahill 7 Oct 2022
+#-----------------------------------------------------------------------
+vbk <- 0.23
+pbig <- 0.05 # Pr(spasmodic cohort)
+surv <- 0.84
+cost <- 0.05 # ?
+seq <- 0 # ?
+
+years <- 1:198
+# create a vector indicating whether a big cohort happen
+# carl does in excel using uniform deviate and algebra
+# but I use rbinom()
+
+set.seed(1)
+R <- rbinom(n = length(years), size = 1, pbig)
+Ut <- rep(0, length(years))
+
+obj <- function(Ut) { # want optimizer to solve for Ut
+  # bring in pars
+  vbk <- leading_pars[1]
+  pbig <- leading_pars[2]
+  surv <- leading_pars[3]
+  cost <- leading_pars[4]
+  seq <- leading_pars[5]
+
+  # set up age of dominant cohort vector
+  Adom <- Wdom <- Ndom <- Nresid <- B <- rep(NA, length(years)) # dom = dominant cohort
+
+  # initialize
+  Adom[1] <- Ndom[1] <- 1
+  Nresid[1] <- 0
+  Wdom[1] <- (1 - exp(-vbk * Adom[1]))^3
+  B[1] <- Ndom[1] * Wdom[1] + Nresid[1]
+
+  for (t in 2:length(years)) {
+    if (R[t - 1] == 0) {
+      Adom[t] <- Adom[t - 1] + 1 # increment age by one year
+      Ndom[t] <- Ndom[t - 1] * surv * (1 - Ut[t - 1])
+      Nresid[t] <- Nresid[t - 1] * surv * (1 - Ut[t - 1]) # carry over residual (if present)
+    } else {
+      Adom[t] <- 1 # new cohort, start a counter over
+      Ndom[t] <- R[t - 1]
+      Nresid[t] <- Ndom[t - 1] * surv * (1 - Ut[t - 1])
+    }
+    Wdom[t] <- (1 - exp(-vbk * Adom[t]))^3
+    B[t] <- Ndom[t] * Wdom[t] + Nresid[t]
+  }
+  if (which_obj == "catch") {
+    obj <- sum(Ut * B) - cost * sum(Ut)
+  }
+  if (which_obj == "utility") {
+    obj <- sum((Ut * B)^0.6) - cost * sum(Ut)
+  }
+  # my_data <- data.frame(years, Ut, Ndom, Nresid, Adom, Wdom, R, B)
+  -obj
+}
+which_obj <- "utility"
+leading_pars <- c(vbk, pbig, surv, cost, seq)
+
+obj(Ut)
+
+# I had to run a few times for utility, catch was easy
+opt <- nlminb(opt$par, obj,
+              control = list(eval.max = 1000, iter.max = 500),
+              lower = rep(0, length(Ut)), upper = rep(1, length(Ut)),
+)
+opt$objective
+
+Ut <- opt$par
+
+plot(my_data$B ~ my_data$years, type = "l", col = "blue", ylim = c(0, 1), 
+     lwd = 1.5, main="total biomass, Rt for Utility objective", ylab="", 
+     xlab = "Year")
+lines(my_data$R ~ my_data$years, type = "l", col = "black", lwd = 1.5)
+lines(my_data$Ut ~ my_data$years, type = "l", col = "orange", lwd = 1.5) 
+legend(x = "topright",          # Position
+       legend = c("biomass", "Recruits", "Ut"),  # Legend texts
+       col = c("blue", "black", "orange"),       # Line colors
+       lwd = 1.5)               
+
+plot(my_data$Adom, my_data$Ut, type="b", main = "Optimum U vs. Adom", 
+     ylab="Optimum Ut", xlab = "Age of Dominant Cohort")
